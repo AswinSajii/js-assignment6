1d4bc35610476f298644e0d9c6ba98123b32d976
Aswin Saji <112844612+AswinSajii@users.noreply.github.com>
2024-02-12 14:29:04 +0530

    js assignment 6

---
+++ mathMethod.js
@@ -0,0 +1,39 @@
+// 1.math.abs
+const absResult = Math.abs(-5); 
+console.log(absResult); 
+
+// 2.math.ceil
+const ceilResult = Math.ceil(4.2); 
+console.log(ceilResult); 
+
+// 3.math.floor
+const floorResult = Math.floor(4.9);
+console.log(floorResult); 
+
+// 4.math.round
+const roundResult = Math.round(4.5); 
+console.log(roundResult); 
+
+// 5.math.max
+const maxResult = Math.max(10, 5, 20); 
+console.log(maxResult); 
+
+// 6.math.mini
+const minResult = Math.min(10, 5, 20); 
+console.log(minResult); 
+
+// 7.math.pow
+const powResult = Math.pow(2, 3); 
+console.log(powResult); 
+
+// 8.math.sqrt
+const sqrtResult = Math.sqrt(16); 
+console.log(sqrtResult); 
+
+// 9.math.trunc
+const truncResult = Math.trunc(4.9); 
+console.log(truncResult); 
+
+// 10.math.random
+const randomResult = Math.random(); 
+console.log(randomResult);
